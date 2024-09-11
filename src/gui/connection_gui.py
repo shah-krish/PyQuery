656fcfae6bc45f5459184ca9d9108b59c35fcbf8
krish <krishshah1411@gmail.com>
2024-09-10 23:55:24 -0400

    little tweak to connection manager

--- src/gui/connection_gui.py
+++ src/gui/connection_gui.py
@@ -18,15 +18,36 @@ class DatabaseConnector:
         window.geometry('900x200')
         window.tk.call('tk', 'scaling', 2.0)
 
-        lbl = Label(window, text="Enter host: ")
-        lbl = Label(window, text="Enter user: ")
-        lbl = Label(window, text="Enter password: ")
-        lbl = Label(window, text="Enter database: ")
-        lbl.grid(column=0, row=0)
-        T = Text(window, height=1, width=30)
-        T.grid(column=1, row=0)
+        lbl_host = Label(window, text="Enter host: ")
+        lbl_user = Label(window, text="Enter user: ")
+        lbl_pass = Label(window, text="Enter password: ")
+        lbl_db = Label(window, text="Enter database: ")
+        lbl_host.grid(column=0, row=0)
+        lbl_user.grid(column=0, row=1)
+        lbl_pass.grid(column=0, row=2)
+        lbl_db.grid(column=0, row=3)
+        T_host = Text(window, height=1, width=30)
+        T_host.grid(column=1, row=0)
+        T_user = Text(window, height=1, width=30)
+        T_user.grid(column=1, row=1)
+        T_pass = Text(window, height=1, width=30)
+        T_pass.grid(column=1, row=2)
+        T_db = Text(window, height=1, width=30)
+        T_db.grid(column=1, row=3)
+        btnOk = Button(window, text="Ok")
+        btnOk.grid(column=0, row=6)
+        btnCancel = Button(window, text="Cancel", command=window.destroy)
+        btnCancel.grid(column=3, row=6)
 
-        # Implement your connection logic here
+        self.mysql_config['host'] = T_host.get("1.0", "end-1c").strip()
+        self.mysql_config['user'] = T_user.get("1.0", "end-1c").strip()
+        self.mysql_config['password'] = T_pass.get("1.0", "end-1c").strip()
+        self.mysql_config['database'] = T_db.get("1.0", "end-1c").strip()
+
+
+        window.mainloop()
+
+
 
     def connect_to_postgres(self):
         # Placeholder method to connect to PostgreSQL
@@ -50,10 +71,7 @@ class DatabaseConnector:
 
 # Define your configurations
 mysql_config = {
-    'host': 'localhost',
-    'user': 'root',
-    'password': 'krish123',
-    'database': 'a02'
+
 }
 
 postgres_config = {
